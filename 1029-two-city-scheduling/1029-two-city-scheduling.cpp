0f7da354d1ad6e0ce903934770a46d4367a5c754
Thushank Sachin Bagal <thushanksachin@gmail.com>
2026-09-02 15:02:41 +0530

    Time: 0 ms (100%), Space: 10.9 MB (82.46%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int n = costs.size()/2;
+        int ans=0;
+        sort(costs.begin(), costs.end(), [](vector<int>& a, vector<int>& b){
+            return (a[0] - a[1])<(b[0] - b[1]);
+        });
+
+        for(int i=0;i<n;i++){
+            ans+=costs[i][0];
+            ans+=costs[i+n][1];
+        }
+        return ans;
+    }
+};
