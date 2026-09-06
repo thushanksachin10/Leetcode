c99e9ded6ba09f29f7f264729f578bf6ae988ab5
Thushank Sachin Bagal <thushanksachin@gmail.com>
2026-09-07 00:31:47 +0530

    Time: 10 ms (49.11%), Space: 16.1 MB (46.62%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.size();
+        vector<bool> dp(n+1, false);
+        dp[0]=true;
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<i;j++){
+                if(dp[j]==true && find(wordDict.begin(), wordDict.end(), s.substr(j, i-j))!=wordDict.end()){
+                    dp[i]=true;
+                }
+            }
+        }
+        return dp[n];
+    }
+};
